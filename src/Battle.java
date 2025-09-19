1f74d9bfc0974f8100963f27a7524c43fd6aa7b4
GreedyGobling <greedygobling@gmail.com>
2025-09-19 13:24:57 +0200

    added ac mechanic from dnd

--- src/Battle.java
+++ src/Battle.java
@@ -1,7 +1,8 @@
 public class Battle {
     public static void Attack(Stats attacker, Stats defender){
         int damage = (int)(Math.random() * (attacker.getMaxAttack() - attacker.getMinAttack() + 1)) + attacker.getMinAttack();
-        damage -= defender.getDefense();
+//        damage -= defender.getDefense();
+        if (defender.getDefense() > damage) damage = 0; // AC from dnd
         if(damage < 0) damage = 0;
         defender.setHp(defender.getHp() - damage);
         if(defender.getHp() < 0) defender.setHp(0);

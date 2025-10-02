9d477c65f16de9dec256b39b5c66e3e2f50d006a
GreedyGobling <greedygobling@gmail.com>
2025-10-02 17:38:42 +0200

    3 x 3 but on 3 but...

--- src/Board.java
+++ src/Board.java
@@ -7,7 +7,7 @@ public class Board {
         for (int i = 0; i < gameboard.length; i++) { // iterate through the rows
             for (int j = 0; j < gameboard.length; j++) { // iterate through the columns
 //                System.out.println("cell" + " " + i + " " + j);
-                System.out.print(gameboard[2][2] + " | ");
+                System.out.print(gameboard[i][0] + " | " + gameboard[i][1] + " | " + gameboard[i][2]);
                 if (i < 1 ) System.out.println("-------");
             }
         }

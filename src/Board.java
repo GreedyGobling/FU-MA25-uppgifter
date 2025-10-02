b78e3f67fed264b3beef49a6b1c94a5e4418df5b
GreedyGobling <greedygobling@gmail.com>
2025-10-02 16:32:48 +0200

    can read board

--- src/Board.java
+++ src/Board.java
@@ -1,13 +1,12 @@
 public class Board {
     char[][] gameboard = new char[3][3];
     public void start() {
-        System.out.println(gameboard);
         printBoard();
     }
-    public void printBoard(){
-        for (int i = 0; i < gameboard.length; i++) {
-            for (int j = 0; j < gameboard.length; j++) {
-                System.out.println(gameboard);
+    public void printBoard(){ // print the gameboard to the console
+        for (int i = 0; i < gameboard.length; i++) { // iterate through the rows
+            for (int j = 0; j < gameboard.length; j++) { // iterate through the columns
+                System.out.println("cell" + i + " " + j + gameboard);
             }
         }
     }

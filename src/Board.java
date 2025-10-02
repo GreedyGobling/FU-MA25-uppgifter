a6aedc648f48845132e738ca5eac1088be1b2c98
GreedyGobling <greedygobling@gmail.com>
2025-10-02 17:36:54 +0200

    have something now

--- src/Board.java
+++ src/Board.java
@@ -1,12 +1,14 @@
 public class Board {
-    char[][] gameboard = new char[3][3];
+    char[][] gameboard = new char[3][3]; // 2d array
     public void start() {
         printBoard();
     }
     public void printBoard(){ // print the gameboard to the console
         for (int i = 0; i < gameboard.length; i++) { // iterate through the rows
             for (int j = 0; j < gameboard.length; j++) { // iterate through the columns
-                System.out.println("cell" + i + " " + j + gameboard);
+//                System.out.println("cell" + " " + i + " " + j);
+                System.out.print(gameboard[2][2] + " | ");
+                if (i < 1 ) System.out.println("-------");
             }
         }
     }

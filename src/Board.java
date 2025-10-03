5c1fd7089ecb036d268beb8537bdbb77ee4b6506
GreedyGobling <greedygobling@gmail.com>
2025-10-03 12:45:05 +0200

    TODO List in board

--- src/Board.java
+++ src/Board.java
@@ -5,9 +5,14 @@ public class Board {
         GameBoard();
         printBoard();
     }
-
+    //TODO: create a method to reset the gameboard
+    //TODO: create a method to check for a win
+    //TODO: create a method to check for a draw
+    //TODO: create a method to place a mark on the gameboard
+    //TODO: create a method to check if a cell is already occupied
+    //TODO: create a method to place a mark on the gameboard
     private void GameBoard() {
-        for (int i = 0; i <gameboard.length; i++) {
+        for (int i = 0; i < gameboard.length; i++) {
             for (int j = 0; j < gameboard.length; j++) {
                 gameboard[i][j] = ' ';
             }
@@ -15,7 +20,7 @@ public class Board {
     }
 
     private void printBoard() { // print the gameboard to the console
-        for (int i = 0; i < gameboard.length; i++) { // through the rows //
+        for (int i = 0; i < gameboard.length; i++) { // through the rows
 //            for (int j = 0; j < gameboard.length; j++) { // through the columns
 //                  System.out.println("cell" + " " + i + " " + j);
             if (i == 0) System.out.println("|-----------|");
@@ -23,6 +28,5 @@ public class Board {
             if (i < 2) System.out.println("|---+---+---|");
             if (i == 2) System.out.println("|-----------|");
         }
-
     }
 }

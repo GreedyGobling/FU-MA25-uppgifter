01cb0f7e55a60ff4c2b47b9f6c045729cc3fda2a
GreedyGobling <greedygobling@gmail.com>
2025-12-17 13:29:57 +0100

    suspend

--- app/src/main/java/com/example/todo/TodoDAO.kt
+++ app/src/main/java/com/example/todo/TodoDAO.kt
@@ -13,14 +13,14 @@ interface TodoDAO {
     //CRUD = CREATE,READ,UPDATE,DELETE => sql = INSERT,SELECT,UPDATE,DELETE
 
     @Insert
-    fun addTodo(item: TodoItem)
+    suspend fun addTodo(item: TodoItem)
 
     @Query("SELECT * FROM todo_items")
     fun getAllTodo(): Flow<List<TodoItem>>
 
     @Update
-    fun updateTodo(item: TodoItem)
+    suspend fun updateTodo(item: TodoItem)
 
     @Delete
-    fun deleteTODO(item: TodoItem)
+    suspend fun deleteTODO(item: TodoItem)
 }
